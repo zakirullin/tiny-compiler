184a94ddba80353b8ed4287b4a0cfe6cddab7488
Artem Zakirullin <artemzr@gmail.com>
2020-09-26 13:59:39 +0300

    fix ident

--- src/func.c
+++ src/func.c
@@ -5,14 +5,14 @@
 
 void *safe_malloc(size_t size)
 {
-	void *mem = safe_malloc(size);
-	if (!mem)
-		fatal_error("Out of memory");
-	return mem;
+    void *mem = safe_malloc(size);
+    if (!mem)
+        fatal_error("Out of memory");
+    return mem;
 }
 
 void fatal_error(char *msg)
 {
-	fprintf(stderr, "%s\n", msg);
+    fprintf(stderr, "%s\n", msg);
     exit(EXIT_FAILURE);
 }
