64888dd89ceff98b4dfafc285a3631f40052d27d
Artem Zakirullin <artemzr@gmail.com>
2020-09-26 13:57:53 +0300

    add func.c

---
+++ src/func.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "func.h"
+
+void *safe_malloc(size_t size)
+{
+	void *mem = safe_malloc(size);
+	if (!mem)
+		fatal_error("Out of memory");
+	return mem;
+}
+
+void fatal_error(char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+    exit(EXIT_FAILURE);
+}
---
+++ src/func.h
@@ -0,0 +1,7 @@
+#ifndef FUN_H
+#define FUN_H
+
+void *safe_malloc(size_t size);
+void fatal_error(char *msg);
+
+#endif
